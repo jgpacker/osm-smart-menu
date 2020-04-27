8f41dc04b16dc954ed0074fd84766392dd9f93eb
jgpacker <john.packer7@gmail.com>
2020-04-27 00:17:40 -0300

    refactor:Move extractors to sites-configuration.ts
    It's easier to maintain this configuration by keeping it centralized.
    Fortunately that's now possible, thanks to Webpack.

--- lib/background/injectable-content-script.ts
+++ lib/background/injectable-content-script.ts
@@ -1,135 +1,27 @@
 import { browser } from 'webextension-polyfill-ts';
-
-type ExtractorType = {
-  getPermalink?: (document: Document) => string | undefined;
-  getValues?: () => Record<string, string> | undefined;
-}
+import { Sites } from '../sites-configuration';
 
 export type ExtractedData = {
   permalink?: string;
   additionalValues?: Record<string, string>;
 }
 
-// note that the response given to the main thread should contain URL-encoded values (only strings)
-const Extractors: Record<string, ExtractorType> = {
-  howdidyoucontribute: {
-    getPermalink: getPermalinkBySelector('a[href*="//hdyc.neis-one.org/?"]')
-  },
-  bingmaps: {
-    getValues: function(){
-      // Known bug:
-      // The URL doesn't change automatically. If the user enters into //www.bing.com/maps (without parameters) and
-      //   doesn't move the map around at least once, then this script won't be able to extract any information.
-      if (window.history && window.history.state && window.history.state.MapModeStateHistory && window.history.state.MapModeStateHistory.centerPoint) {
-        const mapState = window.history.state.MapModeStateHistory;
-        return {
-          lat: mapState.centerPoint.latitude,
-          lon: mapState.centerPoint.longitude,
-          zoom: mapState.level
-        };
-      }
-      return;
-    }
-  },
-  overpassapi: {
-    getPermalink: openLayers_getPermalink(),
-      //TODO: getValues - we can get userName if it's a changeset analysis and maybe map coordinates on both cases
-  },
-  whodidit: {
-      //TODO: getValues - we may get an username or changeset info
-    getPermalink: openLayers_getPermalink()
-  },
-  osmrelationanalyzer: {
-      //TODO: getValues - we can get userName if it's a changeset analysis and maybe map coordinates on both cases
-  },
-  osmroutemanager: {
-      //TODO: getValues - get user that change this relation for the last time
-  },
-  osmose: {
-    getPermalink: getPermalinkBySelector("[class*=permalink] a"),
-      //TODO: getValues - get parameters from URL because there is a language prefix between /map and /#zoom
-  },
-  osmhistoryviewer: {
-      //TODO: getValues - we can get userName if it's a changeset analysis and maybe map coordinates on both cases
-  },
-  osminspector: {
-    getPermalink: getPermalinkBySelector("a#permalink")
-  },
-  keepright: {
-    getPermalink: openLayers_getPermalink()
-  },
-  opencyclemap: {
-    getPermalink: getPermalinkBySelector("a#permalink")
-  },
-  openseamap: {
-    getPermalink: openLayers_getPermalink()
-  },
-  opensnowmap: {
-    getPermalink: getPermalinkBySelector("a#permalink")
-  },
-  historicmap: {
-    getPermalink: getPermalinkBySelector("a#permalink")
-  },
-  openwhatevermap: {
-    getPermalink: openLayers_getPermalink()
-  },
-  openptmap: {
-    getPermalink: openLayers_getPermalink()
-  },
-  opnvkarte: {
-    getPermalink: openLayers_getPermalink()
-  },
-  openmapsurfer: {
-    getPermalink: openLayers_getPermalink()
+function extractData(siteId: string): ExtractedData {
+  const currentSiteExtractors = Sites[siteId] && Sites[siteId].extractors;
+  if (currentSiteExtractors) {
+    const permalink = currentSiteExtractors.getPermalink && currentSiteExtractors.getPermalink(document);
+    const additionalValues = currentSiteExtractors.getValues && currentSiteExtractors.getValues();
+    return { permalink, additionalValues };
+  } else {
+    return {};
   }
 }
-
-
-function getPermalinkBySelector(selector: string){
-  return function(document: Document){
-      const permalink = document.querySelector(selector) as HTMLAnchorElement;
-      return permalink && permalink.href;
-  }
-}
-
-function openLayers_getPermalink(){
-  return getPermalinkBySelector("[id*=Permalink] a");
-}
-
-// function getPermalinkByValue(){
-//   return function(document: Document){
-//     const permalinkAnchor = [...(document.querySelectorAll('a'))]
-//                             .find(a => a.textContent && /permalink/i.test(a.textContent));
-//     return permalinkAnchor && permalinkAnchor.href;
-//   }
-// }
-
-function extraData(siteId: string): ExtractedData {
-  let permalink;
-  let additionalValues;
-  const currentSite = Extractors[siteId];
-
-  if (currentSite) {
-    if (currentSite.getPermalink) {
-      permalink = currentSite.getPermalink(document);
-    }
-    if (currentSite.getValues) {
-      additionalValues = currentSite.getValues();
-    }
-  }
-
-  return {
-    permalink: permalink,
-    additionalValues: additionalValues
-  };
-}
-
 
 (function(){
   browser.runtime.onMessage.addListener(async function(message: { id: string }, _sender) {
     console.debug("injected content script received request: " + JSON.stringify(message));
   
     const siteId = message.id;
-    return extraData(siteId);
+    return extractData(siteId);
   });
 })();
--- lib/sites-configuration.ts
+++ lib/sites-configuration.ts
@@ -1,39 +1,49 @@
+const naturalNumberRegExp = "[0-9]+";
+const decimalNumberRegExp = "[0-9.-]+";
+const positiveDecimalNumberRegExp = "[0-9.]+";
+
 export const InfoRegExp: Record<string, string> = {
-  //link       : {regExp: ".+"},
-  nodeId     : "[0-9]+",
-  wayId      : "[0-9]+",
-  relationId : "[0-9]+",
-  tracesId   : "[0-9]+",
-  userId     : "[0-9]+", // id bigint NOT NULL,
-  userName   : "[^#?\/]+", // display_name character varying DEFAULT ''::character varying NOT NULL, https://github.com/openstreetmap/openstreetmap-website/blob/master/app/models/user.rb#L37-L46
-  changesetId: "[0-9]+",
-  zoom       : "[0-9.]+", //believe it or not, some websites accept a decimal zoom. TODO: verify if any site has a problem having a decimal zoom as a parameter
-  lat        : "[0-9-.]+",
-  lon        : "[0-9-.]+",
-  key        : "[^#?\/=]+",
-  value      : "[^#?\/]+"
+  //link: ".+",
+  nodeId: naturalNumberRegExp,
+  wayId: naturalNumberRegExp,
+  relationId: naturalNumberRegExp,
+  tracesId: naturalNumberRegExp,
+  userId: naturalNumberRegExp, // id bigint NOT NULL,
+  userName: "[^#?\/]+", // display_name character varying DEFAULT ''::character varying NOT NULL, https://github.com/openstreetmap/openstreetmap-website/blob/master/app/models/user.rb#L37-L46
+  changesetId: naturalNumberRegExp,
+  zoom: positiveDecimalNumberRegExp, //believe it or not, some websites accept a decimal zoom. TODO: verify if any site has a problem having a decimal zoom as a parameter
+  lat: decimalNumberRegExp,
+  lon: decimalNumberRegExp,
+  key: "[^#?\/=]+",
+  value: "[^#?\/]+"
 };
 //TODO: should I add support for route information? (start, intermediary and end points, and maybe transport mode)
+
+export type Extractors = {
+  getPermalink?: (document: Document) => string | undefined;
+  getValues?: () => Record<string, string> | undefined;
+};
 
 export type SiteConfiguration = {
   link: string;
   paramOpts: ParamOpt[];
+  extractors?: Extractors;
 }
 
 export type ParamOpt = {
   ordered: string;
   unordered?: {
-      zoom?: string;
-      lat?: string;
-      lon?: string;
-      changesetId?: string;
-      wayId?: string;
-      nodeId?: string;
-      relationId?: string;
+    zoom?: string;
+    lat?: string;
+    lon?: string;
+    changesetId?: string;
+    wayId?: string;
+    nodeId?: string;
+    relationId?: string;
   };
 }
 
-const urlPattern1: ParamOpt = {ordered:"/", unordered: {zoom:"zoom", lat:"lat", lon: "lon"}};
+const urlPattern1: ParamOpt = { ordered: "/", unordered: { zoom: "zoom", lat: "lat", lon: "lon" } };
 
 //TODO:
 // * document it in a schema
@@ -45,98 +55,138 @@ export const Sites: Record<string, SiteConfiguration> = {
     link: "openstreetmap.org",
     //icon: "www.openstreetmap.org/favicon.ico", // TODO: I will need to pre-download this because otherwise I need additional security permissions in the CSP
     paramOpts: [ // TODO: should I add {domain} at the start? it may be useful for sites that add something in a subdomain
-      {ordered:"/node/{nodeId}#map={zoom}/{lat}/{lon}"},
-      {ordered:"/node/{nodeId}"},
-      {ordered:"/way/{wayId}#map={zoom}/{lat}/{lon}"},
-      {ordered:"/way/{wayId}"},
-      {ordered:"/relation/{relationId}#map={zoom}/{lat}/{lon}"}, //TODO: should I create a space to allow the existence of other parameters? For example to recognize http://www.openstreetmap.org/way/263290462?locale=pt#map=17/-26.30144/-48.84531
-      {ordered:"/relation/{relationId}"},
-      {ordered:"/changeset/{changesetId}#map={zoom}/{lat}/{lon}"},
-      {ordered:"/changeset/{changesetId}"},
-      {ordered:"/user/{userName}/traces/{tracesId}"},
-      {ordered:"/user/someone/traces/{tracesId}"}, //when there is no {userName} data, but there is {tracesId}, because the userName doesn't really matter here when going to this page
-      {ordered:"/user/{userName}"},
-      {ordered:"/#map={zoom}/{lat}/{lon}"},
-      {ordered:"/", unordered: {lat:"mlat", lon:"mlon"}}
+      { ordered: "/node/{nodeId}#map={zoom}/{lat}/{lon}" },
+      { ordered: "/node/{nodeId}" },
+      { ordered: "/way/{wayId}#map={zoom}/{lat}/{lon}" },
+      { ordered: "/way/{wayId}" },
+      { ordered: "/relation/{relationId}#map={zoom}/{lat}/{lon}" }, //TODO: should I create a space to allow the existence of other parameters? For example to recognize http://www.openstreetmap.org/way/263290462?locale=pt#map=17/-26.30144/-48.84531
+      { ordered: "/relation/{relationId}" },
+      { ordered: "/changeset/{changesetId}#map={zoom}/{lat}/{lon}" },
+      { ordered: "/changeset/{changesetId}" },
+      { ordered: "/user/{userName}/traces/{tracesId}" },
+      { ordered: "/user/someone/traces/{tracesId}" }, //when there is no {userName} data, but there is {tracesId}, because the userName doesn't really matter here when going to this page
+      { ordered: "/user/{userName}" },
+      { ordered: "/#map={zoom}/{lat}/{lon}" },
+      { ordered: "/", unordered: { lat: "mlat", lon: "mlon" } }
     ]
   },
 
   bingmaps: {
     link: "www.bing.com",
     paramOpts: [
-      {ordered: "/mapspreview?cp={lat}~{lon}&lvl={zoom}"} // this CenterPoint syntax can be tricky...
-    ]
+      { ordered: "/mapspreview?cp={lat}~{lon}&lvl={zoom}" } // this CenterPoint syntax can be tricky...
+    ],
+    extractors: {
+      getValues: function () {
+        // Known bug:
+        // The URL doesn't change automatically. If the user enters into //www.bing.com/maps (without parameters) and
+        //   doesn't move the map around at least once, then this script won't be able to extract any information.
+        if (window.history && window.history.state && window.history.state.MapModeStateHistory && window.history.state.MapModeStateHistory.centerPoint) {
+          const mapState = window.history.state.MapModeStateHistory;
+          return {
+            lat: mapState.centerPoint.latitude,
+            lon: mapState.centerPoint.longitude,
+            zoom: mapState.level
+          };
+        }
+        return;
+      }
+    },
   },
 
   googlemaps: { // there is also maps.google.fr and so on
     link: "www.google.com/maps", //redirected from maps.google.com
     // TODO: otherDomainsRegExp: /.*.google.*/, // not sure whether that's the best way to go about it, but whatever
     paramOpts: [
-      {ordered: "/@{lat},{lon},{zoom}z"}
+      { ordered: "/@{lat},{lon},{zoom}z" }
     ]
   },
 
   openmapsurfer: {
     link: "korona.geog.uni-heidelberg.de",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: openLayers_getPermalink()
+    }
   },
 
   opencyclemap: {
     link: "www.opencyclemap.org",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: getPermalinkBySelector("a#permalink")
+    },
   },
 
   openseamap: {
     link: "map.openseamap.org",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: openLayers_getPermalink()
+    },
   },
 
   opensnowmap: {
     link: "www.opensnowmap.org",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: getPermalinkBySelector("a#permalink")
+    },
   },
 
   historicmap: {
     link: "gk.historic.place/historische_objekte",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: getPermalinkBySelector("a#permalink")
+    },
   },
 
   openwhatevermap: {
     link: "www.openwhatevermap.org",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: openLayers_getPermalink()
+    },
   },
 
   openptmap: {
     link: "www.openptmap.org",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: openLayers_getPermalink()
+    },
   },
 
   opnvkarte: {
     link: "www.öpnvkarte.de",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: openLayers_getPermalink()
+    },
   },
 
   stamen: { // Note: no permalink, so if an user enters into the site by a link without parameters and doesn't move around at least once, then we don't have access to current coordinates
     link: "maps.stamen.com",
     paramOpts: [
-      {ordered:"/#terrain/{zoom}/{lat}/{lon}"}, // wasn't able to find a generic URL, so this one was arbitrarily chosen as the first
-      {ordered:"/#watercolor/{zoom}/{lat}/{lon}"},
-      {ordered:"/#toner/{zoom}/{lat}/{lon}"},
+      { ordered: "/#terrain/{zoom}/{lat}/{lon}" }, // wasn't able to find a generic URL, so this one was arbitrarily chosen as the first
+      { ordered: "/#watercolor/{zoom}/{lat}/{lon}" },
+      { ordered: "/#toner/{zoom}/{lat}/{lon}" },
     ]
   },
 
   f4map: {
     link: "demo.f4map.com",
     paramOpts: [
-      {ordered:"/#lat={lat}&lon={lon}&zoom={zoom}"} //there are other attributes that can be added if another website with 3D rendering shows up: &camera.theta=57.319&camera.phi=-2.005
+      { ordered: "/#lat={lat}&lon={lon}&zoom={zoom}" } //there are other attributes that can be added if another website with 3D rendering shows up: &camera.theta=57.319&camera.phi=-2.005
     ]
   },
 
   opensciencemap: {
     link: "opensciencemap.org/map",
     paramOpts: [
-      {ordered:"/#&scale={zoom}&lat={lat}&lon={lon}"}, //go-to option
-      {ordered:"/#", unordered: {zoom:"scale", lat:"lat", lon: "lon"}} //maximum recognizability option
+      { ordered: "/#&scale={zoom}&lat={lat}&lon={lon}" }, //go-to option
+      { ordered: "/#", unordered: { zoom: "scale", lat: "lat", lon: "lon" } } //maximum recognizability option
     ]
     //TODO: has other attributes e.g. rot and tilt
     // example: http://opensciencemap.org/map/#&scale=17&rot=0&tilt=0&lat=-26.253&lon=-48.854
@@ -145,31 +195,31 @@ export const Sites: Record<string, SiteConfiguration> = {
   hotmap: {
     link: "map.hotosm.org",
     paramOpts: [
-      {ordered:"/#{zoom}/{lat}/{lon}"}
+      { ordered: "/#{zoom}/{lat}/{lon}" }
     ]
   },
 
   mapillary: {
     link: "mapillary.com",
     paramOpts: [ // Note: has a decimal zoom and numbers with high precision (15 digits)
-      {ordered:"/app", unordered: {zoom:"z", lat:"lat", lon: "lng"}}
+      { ordered: "/app", unordered: { zoom: "z", lat: "lat", lon: "lng" } }
     ]
   },
 
   idlatest: {
     link: "openstreetmap.us/iD/release",
     paramOpts: [ // Note: has a decimal zoom and has an unusual order for latitude and longitude
-      {ordered:"#map={zoom}/{lon}/{lat}"}, //go-to option
-      {ordered:"map={zoom}/{lon}/{lat}"} //maximum recognizability option
+      { ordered: "#map={zoom}/{lon}/{lat}" }, //go-to option
+      { ordered: "map={zoom}/{lon}/{lat}" } //maximum recognizability option
     ]
   },
 
   level0: {
     link: "level0.osmz.ru",
     paramOpts: [
-      {ordered:"/?url=n{nodeId}"},
-      {ordered:"/?url=w{wayId}!"},
-      {ordered:"/?url=r{relationId}"},
+      { ordered: "/?url=n{nodeId}" },
+      { ordered: "/?url=w{wayId}!" },
+      { ordered: "/?url=r{relationId}" },
       //In the future, there might be a permalink for the mini-map: https://github.com/Zverik/Level0/issues/16
     ]
   },
@@ -177,16 +227,16 @@ export const Sites: Record<string, SiteConfiguration> = {
   rawedit: {
     link: "rawedit.openstreetmap.fr",
     paramOpts: [
-      {ordered:"/edit/node/{nodeId}"},
-      {ordered:"/edit/way/{wayId}"},
-      {ordered:"/edit/relation/{relationId}"}
+      { ordered: "/edit/node/{nodeId}" },
+      { ordered: "/edit/way/{wayId}" },
+      { ordered: "/edit/relation/{relationId}" }
     ]
   },
 
   umap: {
     link: "umap.openstreetmap.fr",
     paramOpts: [
-      {ordered:"/#{zoom}/{lat}/{lon}"} // add {anything} and {lang} flags
+      { ordered: "/#{zoom}/{lat}/{lon}" } // add {anything} and {lang} flags
       //add http://umap.openstreetmap.fr/pt/map/testpk_1#13/48.2057/-4.0259
     ]
   },
@@ -194,121 +244,139 @@ export const Sites: Record<string, SiteConfiguration> = {
   osmdeephistory: {
     link: "osmlab.github.io/osm-deep-history",
     paramOpts: [
-      {ordered:"/#/node/{nodeId}"},
-      {ordered:"/#/way/{wayId}"},
-      {ordered:"/#/relation/{relationId}"}
+      { ordered: "/#/node/{nodeId}" },
+      { ordered: "/#/way/{wayId}" },
+      { ordered: "/#/relation/{relationId}" }
     ]
   },
 
   deepdiff: {
     link: "osm.mapki.com",
     paramOpts: [
-      {ordered:"/history/node.php", unordered: {nodeId: "id"}},
-      {ordered:"/history/way.php", unordered: {wayId: "id"}},
-      {ordered:"/history/relation.php", unordered: {relationId: "id"}}
+      { ordered: "/history/node.php", unordered: { nodeId: "id" } },
+      { ordered: "/history/way.php", unordered: { wayId: "id" } },
+      { ordered: "/history/relation.php", unordered: { relationId: "id" } }
     ]
   },
 
   osmhistoryviewer: {
     link: "osmhv.openstreetmap.de",
     paramOpts: [
-      {ordered: "/changeset.jsp", unordered: {changesetId: "id"}},
-      {ordered: "/blame.jsp", unordered: {relationId: "id"}}
-    ]
+      { ordered: "/changeset.jsp", unordered: { changesetId: "id" } },
+      { ordered: "/blame.jsp", unordered: { relationId: "id" } }
+    ],
+    extractors: {
+      //TODO: getValues - we can get userName if it's a changeset analysis and maybe map coordinates on both cases
+    },
   },
 
   whodidit: {
     link: "zverik.osm.rambler.ru/whodidit",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      //TODO: getValues - we may get an username or changeset info
+      getPermalink: openLayers_getPermalink()
+    },
   },
 
   overpassapi: {
     link: "overpass-api.de/achavi",
     paramOpts: [
-      {ordered: "/", unordered: {changesetId: "changeset", zoom: "zoom", lat: "lat", lon: "lon"}},
-      {ordered: "/", unordered: {changesetId: "changeset"}}
-    ]
+      { ordered: "/", unordered: { changesetId: "changeset", zoom: "zoom", lat: "lat", lon: "lon" } },
+      { ordered: "/", unordered: { changesetId: "changeset" } }
+    ],
+    extractors: {
+      getPermalink: openLayers_getPermalink(),
+      //TODO: getValues - we can get userName if it's a changeset analysis and maybe map coordinates on both cases
+    },
   },
 
   osmrelationanalyzer: {
     link: "ra.osmsurround.org",
     paramOpts: [
-      {ordered: "/analyzeRelation", unordered: {relationId: "relationId"}}
-    ]
+      { ordered: "/analyzeRelation", unordered: { relationId: "relationId" } }
+    ],
+    extractors: {
+      //TODO: getValues - we can get userName if it's a changeset analysis and maybe map coordinates on both cases
+    },
   },
 
   osmroutemanager: {
     link: "osmrm.openstreetmap.de",
     paramOpts: [
-      {ordered: "/relation.jsp", unordered: {relationId: "id"}}
-    ]
+      { ordered: "/relation.jsp", unordered: { relationId: "id" } }
+    ],
+    extractors: {
+      //TODO: getValues - get user that change this relation for the last time
+    },
   },
 
   osmose: { // Note: has support for languages
     link: "osmose.openstreetmap.fr/map",
     paramOpts: [
-      {ordered: "/#zoom={zoom}&lat={lat}&lon={lon}"},
-    ]
+      { ordered: "/#zoom={zoom}&lat={lat}&lon={lon}" },
+    ],
+    extractors: {
+      getPermalink: getPermalinkBySelector("[class*=permalink] a"),
+      //TODO: getValues - get parameters from URL because there is a language prefix between /map and /#zoom
+    },
   },
 
   osminspector: {
     link: "tools.geofabrik.de/osmi",
-    paramOpts: [ urlPattern1 ]
+    paramOpts: [urlPattern1],
+    extractors: {
+      getPermalink: getPermalinkBySelector("a#permalink")
+    },
   },
 
   mapcompare: {
     link: "tools.geofabrik.de/mc",
     paramOpts: [
-      {ordered:"/#{zoom}/{lat}/{lon}"}
+      { ordered: "/#{zoom}/{lat}/{lon}" }
     ]
   },
-
-/*
-  keepright: {
-    link: "keepright.at",
-    paramOpts: [
-      {ordered: "/report_map.php"}, //TODO: parameters...
-    ]
-  },
-*/
 
   howdidyoucontribute: {
     link: "hdyc.neis-one.org",
     paramOpts: [
-      {ordered: "/?{userName}" }
-    ]
+      { ordered: "/?{userName}" }
+    ],
+    extractors: {
+      getPermalink: getPermalinkBySelector('a[href*="//hdyc.neis-one.org/?"]'),
+    },
   },
 
   osmwiki: {
     link: "wiki.openstreetmap.org",
     paramOpts: [
-      {ordered: "/Tag:{key}={value}"},
-      {ordered: "/Key:{key}"}
+      { ordered: "/Tag:{key}={value}" },
+      { ordered: "/Key:{key}" }
     ]
   },
 
   neistools: {
     link: "resultmaps.neis-one.org",
     paramOpts: [
-      {ordered: "/osm-change-viz?c={changesetId}"},
-      {ordered: "/osm-change-viz.php?c={changesetId}"},
-      {ordered: "/osm-change-tiles#{zoom}/{lat}/{lon}"},
-      {ordered: "/osm-change-tiles.php#{zoom}/{lat}/{lon}"}
+      { ordered: "/osm-change-viz?c={changesetId}" },
+      { ordered: "/osm-change-viz.php?c={changesetId}" },
+      { ordered: "/osm-change-tiles#{zoom}/{lat}/{lon}" },
+      { ordered: "/osm-change-tiles.php#{zoom}/{lat}/{lon}" }
     ]
   },
 
   taginfo: {
     link: "taginfo.openstreetmap.org",
     paramOpts: [
-      {ordered: "/tags/{key}={value}"},
-      {ordered: "/keys/{key}"}
+      { ordered: "/tags/{key}={value}" },
+      { ordered: "/keys/{key}" }
     ]
   }
 };
-  //http://brouter.de/brouter-web/#zoom=6&lat=50.99&lon=9.86&layer=OpenStreetMap
-  //Overpass Turbo Wizard: http://overpass-turbo.eu/?w=%22area%22%3D%22y%22+global
-  //http://maproulette.org/
-  //http://ris.dev.openstreetmap.org/tsbp-proto/{tracesId}/6/1/  from http://wiki.openstreetmap.org/wiki/That_Shouldnt_Be_Possible
+//http://brouter.de/brouter-web/#zoom=6&lat=50.99&lon=9.86&layer=OpenStreetMap
+//Overpass Turbo Wizard: http://overpass-turbo.eu/?w=%22area%22%3D%22y%22+global
+//http://maproulette.org/
+//http://ris.dev.openstreetmap.org/tsbp-proto/{tracesId}/6/1/  from http://wiki.openstreetmap.org/wiki/That_Shouldnt_Be_Possible
 
 /*
   josm: {
@@ -319,3 +387,34 @@ export const Sites: Record<string, SiteConfiguration> = {
   },
 
 */
+/*
+  keepright: {
+    link: "keepright.at",
+    paramOpts: [
+      { ordered: "/report_map.php" }, //TODO: parameters...
+    ],
+    extractors: {
+      getPermalink: openLayers_getPermalink()
+    },
+  },
+*/
+
+function getPermalinkBySelector(selector: string) {
+  return function (document: Document) {
+    const permalink = document.querySelector(selector) as HTMLAnchorElement;
+    return permalink && permalink.href;
+  }
+}
+
+
+function openLayers_getPermalink() {
+  return getPermalinkBySelector("[id*=Permalink] a");
+}
+
+// function getPermalinkByValue(){
+//   return function(document: Document){
+//     const permalinkAnchor = [...(document.querySelectorAll('a'))]
+//                             .find(a => a.textContent && /permalink/i.test(a.textContent));
+//     return permalinkAnchor && permalinkAnchor.href;
+//   }
+// }
