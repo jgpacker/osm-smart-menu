6e8a24a4fa82fa2c8a085eaf3f8d698c1b0f5739
jgpacker <john.packer7@gmail.com>
2020-05-15 23:27:41 -0300

    refactor: small adjustment in webpack config
    This will increase the extension's size significantly, but takes away
    one step out of publishing for Firefox, and may make it easier to
    publish an update to Chrome

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  optimization: {
+    minimize: false, // ease code review by webextension stores
+  },
   entry: {
     "injectable-content-script": "./src/injectable-content-script.ts",
     "popup/script": "./src/popup/script.ts"
@@ -20,5 +23,5 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
-  }
+  },
 };
