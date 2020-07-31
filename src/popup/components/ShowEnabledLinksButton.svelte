f4a1c5d4b47a2c9bd5f213787515115cf6b1739d
jgpacker <john.packer7@gmail.com>
2020-07-31 20:14:29 -0300

    feat:Show all enabled links when requested
    Before, only enabled map links were shown

--- src/popup/components/ShowEnabledLinksButton.svelte
+++ src/popup/components/ShowEnabledLinksButton.svelte
@@ -4,17 +4,49 @@
   import type { SiteLink } from "../sites-manipulation-helper";
   import { getSitesConfiguration } from "../../storage/config-handler";
   import LinkList from "./LinkList.svelte";
+  import { OsmAttribute } from "../../sites-configuration";
 
-  let maybeEnabledLinks: SiteLink[] | undefined;
+  let enabledLinks: SiteLink[] | undefined;
 
   async function getEnabledLinks(): Promise<SiteLink[]> {
-    const basicData: Record<string, string> = {
+    const config = await getSitesConfiguration();
+    const basicMapParameters = {
       zoom: "3",
       lat: "23.00",
       lon: "24.43",
     };
-    const config = await getSitesConfiguration();
-    return getRelevantSites(config, undefined, basicData);
+    const enabledMapLinks = getRelevantSites(
+      config,
+      undefined,
+      basicMapParameters
+    );
+    const allBasicParameters: Record<OsmAttribute, string> = {
+      ...basicMapParameters,
+      nodeId: "1",
+      wayId: "1",
+      relationId: "1",
+      changesetId: "1",
+      userName: "jgpacker",
+      key: "amenity",
+      value: "school",
+      tracesId: "1",
+    };
+    const allEnabledLinks: SiteLink[] = getRelevantSites(
+      config,
+      undefined,
+      allBasicParameters
+    );
+
+    allEnabledLinks.forEach((link, linkIndex) => {
+      const possibleMapLink = enabledMapLinks.find(
+        (mapLink) => mapLink.id === link.id
+      );
+      if (possibleMapLink) {
+        // prioritize the "map versions" of each link
+        allEnabledLinks[linkIndex] = possibleMapLink;
+      }
+    });
+    return allEnabledLinks;
   }
 </script>
 
@@ -29,13 +61,12 @@
   }
 </style>
 
-{#if !maybeEnabledLinks}
+{#if enabledLinks}
+  <LinkList siteLinks={enabledLinks} />
+{:else}
   <div id="fix-button-margin">
-    <button
-      on:click={async () => (maybeEnabledLinks = await getEnabledLinks())}>
+    <button on:click={async () => (enabledLinks = await getEnabledLinks())}>
       {browser.i18n.getMessage('button_showEnabledLinks')}
     </button>
   </div>
-{:else}
-  <LinkList siteLinks={maybeEnabledLinks} />
 {/if}
